ef2a15effaf5fb54c1e1a442b97fdb2ace384065
Ingi Brown <ingi.brown@gmail.com>
2010-06-18 14:30:09 +0200

    Renamed Constantes to Consts and added public

---
+++ application/Consts.java
@@ -0,0 +1,16 @@
+package application;
+
+public final class Consts {
+	
+ public final static String demandeC0 = "Entrez un C0, tapez Entrée pour valider";
+ public final static String demandeK0 = "Entrez un K0, tapez Entrée pour valider";
+ public final static String demandeCorK = "Pour ajouter une connaissance, tapez K \n Pour partitionner un concept, tapez C \n";
+ public final static String erreur = "Mauvaise saisie !";
+ public final static String taperK = "Entrez un Knowledge, tapez Entrée pour valider";
+ public final static String taperC = "Entrez un Concept, tapez Entrée pour valider";
+ public final static String lienC = "Si cette connaissance à un lien avec un des concept suivants, entrez le numéro du concept, sinon tapez -1, suivi d'Entrée";
+ public final static String partC = "Quel concept voulez vous partitionner? Entrez le numéro et tapez Entrée";
+ public final static String partK = "Quelle connaissance vous permet de partitionner ?" ;
+ public final static String partK2 = " Entrez le numéro et tapez Entrée ou -1 ajouter une connaissance manquante";
+ public final static String continuer = "Voulez-vous continuer ?";
+}
