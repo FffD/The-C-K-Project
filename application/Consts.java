692ff2cedb5f581bcbc1ec93547cee809e02baee
Ingi Brown <ingi.brown@gmail.com>
2010-06-19 14:33:35 +0200

    Changed accented characters to hex code

--- application/Consts.java
+++ application/Consts.java
@@ -12,14 +12,14 @@ public final class Consts {
 	
 	
 	/** Input Strings **/
-	public final static String initialC = "Entrez un C0, tapez Entrée pour valider";
-	public final static String initialK = "Entrez un K0, tapez Entrée pour valider";
+	public final static String initialC = "Entrez un C0, tapez Entr\u00E9e pour valider";
+	public final static String initialK = "Entrez un K0, tapez Entr\u00E9e pour valider";
 	public final static String inputCorK = "Pour ajouter une connaissance, tapez K \nPour partitionner un concept, tapez C \n";
-	public final static String inputK = "Entrez un Knowledge, tapez Entrée pour valider";
-	public final static String inputC = "Entrez un Concept, tapez Entrée pour valider";
-	public final static String inputLinkCtoK = "Si cette connaissance à un lien avec un des concept suivants, entrez le numéro du concept, sinon tapez -1, suivi d'Entrée";
-	public final static String inputCtoPartition = "Quel concept voulez vous partitionner? Entrez le numéro et tapez Entrée";
-	public final static String inputKtoPartitionC = "Quelle connaissance vous permet de partitionner ?"+NEW_LINE+"Entrez le numéro et tapez Entrée ou -1 ajouter une connaissance manquante";
+	public final static String inputK = "Entrez un Knowledge, tapez Entr\u00E9e pour valider";
+	public final static String inputC = "Entrez un Concept, tapez Entr\u00E9e pour valider";
+	public final static String inputLinkCtoK = "Si cette connaissance \u00E0 un lien avec un des concept suivants, entrez le num\u00E9ro du concept, sinon tapez -1, suivi d'Entr\u00E9e";
+	public final static String inputCtoPartition = "Quel concept voulez vous partitionner? Entrez le num\u00E9ro et tapez Entr\u00E9e";
+	public final static String inputKtoPartitionC = "Quelle connaissance vous permet de partitionner ?"+NEW_LINE+"Entrez le num\u00E9ro et tapez Entr\u00E9e ou -1 ajouter une connaissance manquante";
 	public final static String inputContinueQuestion = "Voulez-vous continuer ?";
 
 	/** Error messages **/
